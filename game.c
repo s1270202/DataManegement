8ac22be36106fe2f5333ff006fe56a91226f8f01
hiroki <hiroki@localhost>
2020-08-06 15:13:09 +0900

    Hot:Initial revision

---
+++ game.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 3
+
+int main () {
+    int i, num;
+    int heads = 0, tails = 0;
+
+    srand((unsigned int)time(NULL));
+
+    printf("Tossing a coin...\n");
+
+    for (i = 0; i < N; i++) {
+        num = rand() % 2;
+
+        printf("Round %d: ", i+1);
+
+        if (num == 0) {
+            printf("Heads\n");
+            heads++;
+        }
+        else if (num == 1) {
+            printf("Tails\n");
+            tails++;
+        }
+    }
+
+    printf("Heads: %d, Tails: %d\n", heads, tails);
+
+    return 0;
+}
